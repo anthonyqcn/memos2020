bd8c59634c9ce1825a875dadd125a38c2df22de1
anthonyqcn <anthonyqcn@gmail.com>
2020-02-05 22:59:56 -0800

    update memos

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './media.css';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import SearchIcon from '@material-ui/icons/Search';
 
 function App() {
 	const [text, setText] = useState('');
 	const [memes, setMemes] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	async function getMemes() {
+		setLoading(true);
+		setMemes([]);
 		const key = 'koPIZe5K7AQl3ZbCeEbNMiLItDyZ1kYF';
 		let url = 'https://api.giphy.com/v1/gifs/search?';
 		url += 'api_key=' + key;
@@ -17,6 +22,7 @@ function App() {
 		const body = await r.json();
 		setMemes(body.data);
 		setText('');
+		setLoading(false);
 	}
 
 	return (
@@ -38,10 +44,11 @@ function App() {
 						color="primary"
 						onClick={getMemes}
 					>
-						Primary
+						<SearchIcon />
 					</Button>
 				</div>
 			</header>
+			{loading && <LinearProgress />}
 			<div className="memes">
 				{memes.map((meme, i) => (
 					<Meme key={i} {...meme} />
@@ -51,6 +58,19 @@ function App() {
 	);
 }
 
+function Meme({ images, title }) {
+	// const {meme} = props
+	const url = images.fixed_height.url;
+	return (
+		<div className="meme" onClick={() => window.open(url, '_blank')}>
+			<div className="meme-title">{title}</div>
+			<img height="200" alt="meme" src={url} />
+		</div>
+	);
+}
+
+/*
+
 function Meme({ title, images }) {
 	return (
 		<div className="meme">
@@ -59,5 +79,6 @@ function Meme({ title, images }) {
 		</div>
 	);
 }
+*/
 
 export default App;
